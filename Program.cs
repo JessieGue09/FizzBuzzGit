86ad1a7c1b17ba2b98bf6684c31acbcc4337233c
JessieGue09 <jessy.v.hdez0028@gmail.com>
2021-02-11 11:11:23 -0700

    Created FizzBuzz in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace FizzBuzzGit
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 1; i <= 100; i ++) // Números del 1 al 100
+            {
+
+                if (i % 3 == 0 && i % 5 == 0) // multiplo de 3 y 5
+                {
+                    Console.WriteLine("FizzBuzz");
+                } 
+                else if(i % 3 == 0) // multiplo de 3
+                {
+                    Console.WriteLine("Fizz");
+
+                }
+                else if (i % 5 == 0) // multiplo de 5
+                {
+                    Console.WriteLine("Buzz");
+
+                } else {
+                
+                    Console.WriteLine(i);
+                }
+                //Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
         }
     }
 }
